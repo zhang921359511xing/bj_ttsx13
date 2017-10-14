12d7d2a91447b2aa66952ab65a85996c78fbaa5f
laoban <921359511@qq.com>
2017-10-14 20:04:01 +0800

    修改bug

--- ttsx/ttsx/urls.py
+++ ttsx/ttsx/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url('^user/$',include('tt_user.urls')),
+    url('^haha/$',include('tt_user.urls')),
 ]
